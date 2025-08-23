9c329968a9b41eb175b00bbcdbf1a1ec1431490d
Justin Wilson <justin.wilson@omibyte.io>
2025-08-22 20:44:33 -0500

    Refactor mutex to use a ticketing mechanism to reduce lock contention. (#61)
    The previous implementation favored faster goroutines which allow slower goroutines to be starved by lock contention. The new implementation uses a simple ticketing mechanism to guarantee that another goroutine waiting on the lock is allowed to acquire the lock when the lock is released.
    
    goir:
    Refactored `AtomicAddIOp` lowering to yield the new value (Go's expectation) instead of the old one (LLVM's behavior).
    
    sync:
    Implemented ticketing mechanism in mutex.
    Added double-lock and double-free panics to mutex implementation.
    Added panic when mutex is attempted to be unlocked by a goroutine other than the one that originally acquired its lock.

--- src/sync/mutex.go
+++ src/sync/mutex.go
@@ -1,25 +1,72 @@
 package sync
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+	"unsafe"
+)
 
 type Mutex struct {
-	state uint32
+	next   uint32
+	owner  uint32
+	ownerg unsafe.Pointer
 }
 
 //sigo:extern gosched runtime.gosched
 func gosched()
 
 func (m *Mutex) Lock() {
-	for !atomic.CompareAndSwapUint32(&m.state, 0, 1) {
+	currg := getg()
+	ownerg := atomic.LoadPointer(&m.ownerg)
+
+	// Detect recursive locking (non-reentrant).
+	if (currg == nil && ownerg != nil) || (currg != nil && ownerg == currg) {
+		panic("sync.Mutex: double lock by same goroutine")
+	}
+
+	// Get a ticket for the current goroutine.
+	ticket := atomic.AddUint32(&m.next, 1) - 1
+
+	// Wait until it's this goroutine's turn to acquire the lock.
+	for atomic.LoadUint32(&m.owner) != ticket {
 		// Yield to run a different goroutine.
 		gosched()
 	}
+
+	// The current goroutine will own the lock.
+	atomic.StorePointer(&m.ownerg, currg)
 }
 
 func (m *Mutex) TryLock() bool {
-	return atomic.CompareAndSwapUint32(&m.state, 0, 1)
+	owner := atomic.LoadUint32(&m.owner)
+	if atomic.LoadUint32(&m.next) != owner {
+		// The lock is already acquired.
+		return false
+	}
+
+	// Attempt to claim the next ticket.
+	if !atomic.CompareAndSwapUint32(&m.next, owner, owner+1) {
+		return false
+	}
+
+	// The current goroutine will own the lock.
+	atomic.StorePointer(&m.ownerg, getg())
+	return true
 }
 
 func (m *Mutex) Unlock() {
-	atomic.StoreUint32(&m.state, 0)
+	if atomic.LoadUint32(&m.owner) == atomic.LoadUint32(&m.next) {
+		// No one holds the lock (no outstanding ticket == owner).
+		panic("sync.Mutex: unlock of unlocked mutex")
+	}
+
+	// The lock can only be unlocked by the original owner. Check this first!
+	if atomic.LoadPointer(&m.ownerg) != getg() {
+		panic("sync.Mutex: unlock by non-owner")
+	}
+
+	// Free the lock from the current owner.
+	atomic.StorePointer(&m.ownerg, nil)
+
+	// Hand off to the next ticket holder (FIFO).
+	atomic.AddUint32(&m.owner, 1)
 }
